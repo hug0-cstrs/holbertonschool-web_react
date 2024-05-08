bbe8b3a4baaa5302be7e8e441f8e1af974465132
hug0-cstrs <hugo.casteras@outlook.fr>
2024-05-08 12:43:14 +0200

    0. Inline styling

---
+++ React_inline_styling/task_0/dashboard/src/CourseList /CourseListRow.js
@@ -0,0 +1,48 @@
+import PropTypes from "prop-types";
+import React from "react";
+
+const rowStyle = {backgroundColor: '#f5f5f5ab'};
+const headerRowStyle = {backgroundColor: '#deb5b545'};
+
+function CourseListRow({ isHeader, textFirstCell, textSecondCell }) {
+  let element;
+
+  if (isHeader === true) {
+    //
+    if (textSecondCell === null) {
+      element = <th colSpan="2">{textFirstCell}</th>;
+    } else {
+      element = (
+        <>
+          <th>{textFirstCell}</th>
+          <th>{textSecondCell}</th>
+        </>
+      );
+    }
+    //
+  } else if (isHeader === false) {
+    element = (
+      <>
+        <td>{textFirstCell}</td>
+        <td>{textSecondCell}</td>
+      </>
+    );
+  }
+
+  let isHeaderStyle = isHeader ? headerRowStyle : rowStyle;
+
+  return <tr style={isHeaderStyle}>{element}</tr>;
+}
+
+CourseListRow.defaultProps = {
+  isHeader: false,
+  textSecondCell: null,
+};
+
+CourseListRow.propTypes = {
+  isHeader: PropTypes.bool,
+  textFirstCell: PropTypes.string.isRequired,
+  textSecondCell: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+};
+
+export default CourseListRow;
